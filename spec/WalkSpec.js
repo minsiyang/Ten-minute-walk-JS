e65a4c185555d64d5e7623db045b4508d7ec22bb
minsi yang <yangminsi@gmail.com>
2020-08-17 11:50:36 +0100

    Added second test, array length is 10, matching direction count not equal, return false, test passed

--- spec/WalkSpec.js
+++ spec/WalkSpec.js
@@ -9,7 +9,7 @@ describe("Walk", function () {
     expect(walk.isTenMinuteWalk(['w', 's'])).toBeFalse();
   });
 
-  it("should return false when 'n' numbers is not match 's', and 'e' numbers not matching 'w' ", function () {
+  it("should return false when array length is 10, but 'n' numbers is not match 's', and 'e' numbers not matching 'w' ", function () {
     expect(walk.isTenMinuteWalk(['w', 's', 'e', 's', 's', 'e', 's', 'w', 'n', 'n'])).toBeFalse();
-  }
+  })
 });
--- src/Walk.js
+++ src/Walk.js
@@ -1,7 +1,13 @@
 class Walk {
   isTenMinuteWalk(array) {
+    let southCount = array.filter(x => x === 's').length
+    let northCount = array.filter(y => y === 'n').length
+
     if (array.length < 10) {
       return false
+    } else if (southCount !== northCount) {
+      return false
     }
+
   }
 };
